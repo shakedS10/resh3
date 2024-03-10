f424d0edbaecfe37964c91c830c5a7909347d06b
shakedS10 <shaked.shvartz@msmail.ariel.ac.il>
2024-03-10 15:07:34 +0200

    DARWIZZY DA GOAT

--- TCP_Receiver.c
+++ TCP_Receiver.c
@@ -9,8 +9,8 @@
 #include "MAXSIZES.h"
 
 void print_stats(clock_t start, clock_t end, int totalReceived) {
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC; 
-    double bandwidth = (totalReceived / 1024.0) / time_taken; 
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC; // in seconds
+    double bandwidth = (totalReceived / 1024.0) / time_taken; // in KB/s
     printf("Time taken: %.2f seconds\n", time_taken);
     printf("Average Bandwidth: %.2f KB/s\n", bandwidth);
 }
@@ -19,12 +19,14 @@ int main(int argc, char *argv[]) {
 
     int port = atoi(argv[1]);
     char *algo = argv[2];
+    // gettings all the parameters from the terminal
 
     printf("Port: %d\n", port);
     printf("Algorithm: %s\n", algo);
 
     struct sockaddr_in receiver, sender;
     int server_socket, client_socket;
+    // creating the socket to be ipv4 and tcp
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Socket creation failed");
         return 1;
@@ -32,16 +34,19 @@ int main(int argc, char *argv[]) {
     receiver.sin_family = AF_INET;
     receiver.sin_addr.s_addr = INADDR_ANY;
     receiver.sin_port = htons(port);
+    // setting the address to be ipv4 and the port to be the one given in the terminal
     if (bind(server_socket, (struct sockaddr *)&receiver, sizeof(receiver)) < 0) {
         perror("Bind failed");
         close(server_socket);
         return 1;
     }
+    // binding the socket to the address
     if (listen(server_socket, 3) < 0) {
         perror("Listen failed");
         close(server_socket);
         return 1;
     }
+    // starting to listen for connections
     printf("Server started listening\n");
     socklen_t addrlen = sizeof(sender);
     if ((client_socket = accept(server_socket, (struct sockaddr *)&sender, &addrlen)) < 0) {
@@ -49,9 +54,9 @@ int main(int argc, char *argv[]) {
         close(server_socket);
         return 1;
     }
-
+    // accepting the connection print the address of the sender
     printf("Connection made with %s:%d\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port));
-
+    // setting the congestion control algorithm
     if (strcmp(algo, "reno") == 0) {
         if (setsockopt(client_socket, IPPROTO_TCP, TCP_CONGESTION, "reno", strlen("reno")) < 0) {
             printf("Invalid algorithm\n");
@@ -72,45 +77,39 @@ int main(int argc, char *argv[]) {
         close(server_socket);
         return 1;
     }
-    clock_t start = clock();
+    clock_t start = clock(); // start the clock
     int totalReceived = 0;
     int counter = 1;
-    char filename[40];
-    sprintf(filename, "received%d.txt", counter);
-    FILE *file = fopen(filename, "w");
-
-    
-        
-
+    char filename[40]; // to store the name of the file
+    sprintf(filename, "received%d.txt", counter); // setting the name of the file
+    FILE *file = fopen(filename, "w"); // creating the file to store the data
     if (file == NULL) {
         perror("Error opening file");
         close(client_socket);
         close(server_socket);
         return 1;
     }
-
-    char buffer[MAX_SIZE];
-    
+    char buffer[MAX_SIZE]; // buffer to store the data of each send
     int bytesRead;
     int onerecv = 0;
     int currentsize = BUFFER_SIZE;
     if(BUFFER_SIZE % MAX_SIZE != 0)
     {
         currentsize = BUFFER_SIZE+MAX_SIZE;
-    }
-    
+    } // setting the size of the file to be the same as the sender incase of extra data extend it by the size of the buffer to fill with blank data
+    // receiving the data and writing it to the file until the data is finished
     while ((bytesRead = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
         if (onerecv > currentsize - MAX_SIZE)
         {         
             counter++;
-            sprintf(filename, "received%d.txt", counter);
-            fclose(file);
-            onerecv = 0;
+            sprintf(filename, "received%d.txt", counter); // setting the name of the file
+            fclose(file); // closing the file
+            onerecv = 0; // reseting the current size of the file to be 0 because its a new file
         }
         if (onerecv == 0 && bytesRead>0)
         {
             
-            file = fopen(filename, "w");
+            file = fopen(filename, "w"); // creating the new file to store the data
             if (file == NULL) {
                 perror("Error opening file");
                 close(client_socket);
@@ -118,9 +117,9 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
-        fwrite(buffer, sizeof(char), bytesRead, file);
-        totalReceived += bytesRead;
-        onerecv += bytesRead;
+        fwrite(buffer, sizeof(char), bytesRead, file); //writing the data to the file
+        totalReceived += bytesRead; // adding the size of the data to the total size of the data received
+        onerecv += bytesRead; // adding the size of the data to the current size of the file
         printf("Received %d bytes\n", onerecv);
         
     }
@@ -129,17 +128,16 @@ int main(int argc, char *argv[]) {
 
     if (bytesRead < 0) {
         perror("Receive failed");
-    }
-    printf("Data received and written\n");
+    } // if the data is not received print an error
+    printf("Data received and written\n"); // print that the data is received and written
+    printf("Total received: %d bytes\n", totalReceived); // print the total size of the data received
+    fclose(file); // close the last file
 
-    printf("Total received: %d bytes\n", totalReceived);
-    fclose(file);
-
-    clock_t end = clock();
-    print_stats(start, end, totalReceived);
+    clock_t end = clock(); //end timer 
+    print_stats(start, end, totalReceived); // print the statistics of the data received
     close(client_socket);
     close(server_socket);
-
+    // close the sockets
     return 0;
 }
 
--- TCP_Sender.c
+++ TCP_Sender.c
@@ -32,10 +32,10 @@ int main(int argc, char *argv[])
     printf("Port: %d\n", port);
     printf("IP: %s\n", ip);
     printf("Algo: %s\n", algo);
-    
+    // gettings all the parameters from the terminal
 
     struct sockaddr_in sender;
-
+    // creating the socket to be ipv4 and tcp
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1)
     {
@@ -52,16 +52,17 @@ int main(int argc, char *argv[])
         close(sock);
         return 1;
     
-    }
+    } //checking if the address is valid
+
     printf("Address set\n");
     if (connect(sock, (struct sockaddr *)&sender, sizeof(sender)) < 0)
     {
         printf("Connection Failed\n");
         close(sock);
         return 1;
-    }
+    }// connecting to the reciver
     printf("Connected\n");
-    if(strcmp(algo, "reno"))
+    if(strcmp(algo, "reno")) //setting the congestion control algorithm
     {
         if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, "reno", strlen("reno")) < 0)
         {
@@ -73,7 +74,7 @@ int main(int argc, char *argv[])
     }
     else
     {
-        if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, "cubic", strlen("cubic")) < 0)
+        if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, "cubic", strlen("cubic")) < 0) //setting the congestion control algorithm
         {
             printf("Invalid algorithm\n");
             close(sock);
@@ -82,44 +83,50 @@ int main(int argc, char *argv[])
     }
     char *fdata = util_generate_random_data(BUFFER_SIZE);
     FILE *file = fopen("a.txt", "w");
+    //creating a file to store the random data
 
     if (file == NULL) {
         perror("Error opening file");
         return 1; // Return an error code
     }
-    fwrite(fdata, sizeof(char), BUFFER_SIZE, file);
+    fwrite(fdata, sizeof(char), BUFFER_SIZE, file); //writing the data to the file
     fclose(file);
-    FILE *fr = fopen("a.txt", "r");
+    FILE *fr = fopen("a.txt", "r"); //opening the file to read the data
     if (fr == NULL) {
         perror("Error opening file");
         return 1; // Return an error code
     }
     fseek(file, 0, SEEK_END);
     long fileSize = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    fseek(file, 0, SEEK_SET); //getting the size of the file altough we know it already it is a good practice to get it from the file
     printf("File size: %ld\n", fileSize);
-    char *data = (char *)malloc(fileSize + 1);
+    char *data = (char *)malloc(fileSize + 1); //allocating memory for the data
      if (data == NULL) {
         perror("Error allocating memory");
         fclose(file);
         return 1;
     }
-    size_t bytesRead = fread(data, sizeof(char), fileSize, file);
+    size_t bytesRead = fread(data, sizeof(char), fileSize, file);  //reading the data from the file
     data[bytesRead] = '\0';
     fclose(file);
     int check = 1;
     int sent = 0;
     int counter = 0;
     int i = 0;
-    while (check == 1)
+    while (check == 1) //a loop to send the data until the input to stop is given
     {
         
         counter = MAX_SIZE;
         i = 1;
-        if(fileSize % counter != 0)
+        //checking if the data is divisible by the buffer size if not then we need to send the remaining data and fill the rest of the buffer with blanks
+        if(fileSize % counter != 0) 
+        {
+            counter = fileSize + MAX_SIZE;
+        }
         {
             i = 0;
         }
+        //sending the data in chunks of the buffer size until the whole data is sent
         while (i <= fileSize / counter)
         {
             
@@ -138,7 +145,7 @@ int main(int argc, char *argv[])
         char act; 
         printf("%d\n", sent);
         printf("Do you want to send more data? (y/n)\n");
-        scanf(" %c", &act);
+        scanf(" %c", &act); //asking the user if they want to send the data again
         if (act == 'n')
         {
             check = 0;
@@ -149,10 +156,10 @@ int main(int argc, char *argv[])
         }
        
     }
-    free(data);
+    free(data); //freeing the memory
     printf("Data sent\n");
     printf("Sent: %d\n", sent);
-    close(sock);
+    close(sock); //closing the socket
     return 0;
     
     
